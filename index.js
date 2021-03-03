5e51c4d227abb968faa0779f5e92baa803725734
Eddges <shivamshekhar.i7@gmail.com>
2021-03-03 14:16:08 +0530

    Link element and attribute created in index.ts

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 "use strict";
 var fullCalendar = function (events) {
     var dayArray = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
     ];
     var eventsArray = {};
     Object.keys(events).forEach(function (iterator) {
-        console.log('iterator: ', iterator);
+        console.log("iterator: ", iterator);
         var newKey = new Date(iterator).toDateString();
         eventsArray[newKey] = events[iterator];
     });
-    var containerDiv = document.querySelector('.full-calendar');
+    var head = document.getElementsByTagName("HEAD")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "./styles/mainFull.css";
+    head.appendChild(link);
+    var containerDiv = document.querySelector(".full-calendar");
     if (containerDiv) {
         containerDiv.innerHTML = "\n            <div class=\"Top\">\n                <i id=\"prevMonth\" class=\"fas fa-chevron-left\"></i>\n                <div class=\"DivCurrent\">\n                    <span class=\"MonthName\"></span>\n                    <span class=\"CurrentDate\"></span>\n                </div>\n\n                <i id=\"nextMonth\" class=\"fas fa-chevron-right\"></i>\n            </div>\n            <div class=\"Labels\">\n                <span class=\"Label\">Sun</span>\n                <span class=\"Label\">Mon</span>\n                <span class=\"Label\">Tue</span>\n                <span class=\"Label\">Wed</span>\n                <span class=\"Label\">Thu</span>\n                <span class=\"Label\">Fri</span>\n                <span class=\"Label\">Sat</span>\n            </div>\n            <div class=\"Dates\"></div>\n            ";
     }
     var renderCalendar = function (contextMonth, contextYear) {
-        var monthNameElement = document.querySelector('.MonthName');
-        var currentDateElement = document.querySelector('.CurrentDate');
-        var datesElement = document.querySelector('.Dates');
-        console.log('ContainerDiv: ', containerDiv);
+        var monthNameElement = document.querySelector(".MonthName");
+        var currentDateElement = document.querySelector(".CurrentDate");
+        var datesElement = document.querySelector(".Dates");
+        console.log("ContainerDiv: ", containerDiv);
         var contextDate = new Date();
         contextDate.setMonth(contextMonth);
         contextDate.setFullYear(contextYear);
@@ -37,13 +43,14 @@ var fullCalendar = function (events) {
         var startDayOfCurrentMonth = firstDayOfThisMonth.getDay();
         var lastDateOfPrevMonth = new Date(2021, contextDate.getMonth(), 0).getDate();
         if (monthNameElement) {
-            monthNameElement.innerHTML = dayArray[contextMonth] + (" " + contextYear);
+            monthNameElement.innerHTML =
+                dayArray[contextMonth] + (" " + contextYear);
         }
         if (currentDateElement) {
             currentDateElement.innerHTML = new Date().toDateString();
         }
         if (datesElement) {
-            datesElement.innerHTML = '';
+            datesElement.innerHTML = "";
             for (var i = lastDateOfPrevMonth - startDayOfCurrentMonth + 1; i <= lastDateOfPrevMonth; i++) {
                 datesElement.innerHTML += "<div class='Date PrevDate'>" + i + "</div>";
             }
@@ -52,11 +59,11 @@ var fullCalendar = function (events) {
                 var today = new Date(contextYear, contextMonth, i);
                 var todayString = today.toDateString();
                 var numberSpan = "<span class='Number'>" + i + "</span>";
-                var redEvent = '';
-                var greenEvent = '';
-                var blueEvent = '';
+                var redEvent = "";
+                var greenEvent = "";
+                var blueEvent = "";
                 if (eventsArray[todayString]) {
-                    console.log('Matched');
+                    console.log("Matched");
                     eventsFlag = true;
                     if (eventsArray[todayString].red) {
                         redEvent = "<span class='EventsElement Red'></span><span class='Hover NoDisplay'>" + eventsArray[todayString].red + "</span>";
@@ -69,33 +76,37 @@ var fullCalendar = function (events) {
                     }
                 }
                 // eventsArray
-                var eventsDiv = "<div class='Events'>" + (redEvent ? redEvent : '') + (greenEvent ? greenEvent : '') + (blueEvent ? blueEvent : '') + "</div>";
-                datesElement.innerHTML += "<div class='Date " + (i === new Date().getDate() && new Date().getMonth() === contextMonth && new Date().getFullYear() === contextYear ? 'Active' : '') + "'>" + numberSpan + (eventsFlag ? eventsDiv : '') + "</div>";
+                var eventsDiv = "<div class='Events'>" + (redEvent ? redEvent : "") + (greenEvent ? greenEvent : "") + (blueEvent ? blueEvent : "") + "</div>";
+                datesElement.innerHTML += "<div class='Date " + (i === new Date().getDate() &&
+                    new Date().getMonth() === contextMonth &&
+                    new Date().getFullYear() === contextYear
+                    ? "Active"
+                    : "") + "'>" + numberSpan + (eventsFlag ? eventsDiv : "") + "</div>";
             }
             for (var i = 1; i <= 7 - lastDayOfThisMonth.getDay() - 1; i++) {
                 datesElement.innerHTML += "<div class='Date NextDate'>" + i + "</div>";
             }
         }
-        document.querySelectorAll('.EventsElement').forEach(function (item) {
-            item.addEventListener('mouseenter', function () {
+        document.querySelectorAll(".EventsElement").forEach(function (item) {
+            item.addEventListener("mouseenter", function () {
                 var _a;
-                (_a = item.nextElementSibling) === null || _a === void 0 ? void 0 : _a.classList.remove('NoDisplay');
+                (_a = item.nextElementSibling) === null || _a === void 0 ? void 0 : _a.classList.remove("NoDisplay");
             });
         });
-        document.querySelectorAll('.EventsElement').forEach(function (item) {
-            item.addEventListener('mouseout', function () {
+        document.querySelectorAll(".EventsElement").forEach(function (item) {
+            item.addEventListener("mouseout", function () {
                 var _a;
-                (_a = item.nextElementSibling) === null || _a === void 0 ? void 0 : _a.classList.add('NoDisplay');
+                (_a = item.nextElementSibling) === null || _a === void 0 ? void 0 : _a.classList.add("NoDisplay");
             });
         });
     };
     var monthCounter = new Date().getMonth();
     var yearCounter = new Date().getFullYear();
     renderCalendar(monthCounter, yearCounter);
-    var prevMonthChevron = document.querySelector('#prevMonth');
-    var nextMonthChevron = document.querySelector('#nextMonth');
+    var prevMonthChevron = document.querySelector("#prevMonth");
+    var nextMonthChevron = document.querySelector("#nextMonth");
     if (prevMonthChevron) {
-        prevMonthChevron.addEventListener('click', function () {
+        prevMonthChevron.addEventListener("click", function () {
             monthCounter--;
             if (monthCounter < 0) {
                 yearCounter--;
@@ -105,7 +116,7 @@ var fullCalendar = function (events) {
         });
     }
     if (nextMonthChevron) {
-        nextMonthChevron.addEventListener('click', function () {
+        nextMonthChevron.addEventListener("click", function () {
             monthCounter++;
             if (monthCounter > 11) {
                 yearCounter++;
--- index.ts
+++ index.ts
@@ -1,32 +1,40 @@
 interface EventObjectInterface {
-    [key : string] : string
+    [key: string]: string;
 }
 
-const fullCalendar = (events: { [key: string]:  EventObjectInterface}) => {
+const fullCalendar = (events: { [key: string]: EventObjectInterface }) => {
+    const dayArray: string[] = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 
-    const dayArray : string[] = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ]
+    const eventsArray: { [key: string]: EventObjectInterface } = {};
+    Object.keys(events).forEach((iterator) => {
+        console.log("iterator: ", iterator);
+        const newKey: string = new Date(iterator).toDateString();
+        eventsArray[newKey] = events[iterator];
+    });
 
-    const eventsArray: { [key: string]: EventObjectInterface } = {}
-    Object.keys(events).forEach(iterator => {
-        console.log('iterator: ', iterator)
-        const newKey: string = new Date(iterator).toDateString()
-        eventsArray[newKey] = events[iterator]
-    })
-    
-    const containerDiv = document.querySelector('.full-calendar')
+    const head = document.getElementsByTagName("HEAD")[0];
+    const link = document.createElement("link");
+
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "./styles/mainFull.css";
+
+    head.appendChild(link);
+
+    const containerDiv = document.querySelector(".full-calendar");
 
     if (containerDiv) {
         containerDiv.innerHTML = `
@@ -49,115 +57,141 @@ const fullCalendar = (events: { [key: string]:  EventObjectInterface}) => {
                 <span class="Label">Sat</span>
             </div>
             <div class="Dates"></div>
-            `
+            `;
     }
-    
-    const renderCalendar: (contextMonth : number, contextYear : number) => void = (contextMonth, contextYear) => {
 
-        const monthNameElement = document.querySelector('.MonthName')
-        const currentDateElement = document.querySelector('.CurrentDate')
-        const datesElement = document.querySelector('.Dates')
+    const renderCalendar: (
+        contextMonth: number,
+        contextYear: number
+    ) => void = (contextMonth, contextYear) => {
+        const monthNameElement = document.querySelector(".MonthName");
+        const currentDateElement = document.querySelector(".CurrentDate");
+        const datesElement = document.querySelector(".Dates");
 
-        console.log('ContainerDiv: ', containerDiv)
-    
+        console.log("ContainerDiv: ", containerDiv);
+
         const contextDate: Date = new Date();
-    
-        contextDate.setMonth(contextMonth)
-        contextDate.setFullYear(contextYear)
-    
-        const firstDayOfThisMonth : Date = new Date(contextYear, contextDate.getMonth(), 1)
-        const lastDayOfThisMonth : Date = new Date(contextYear, contextDate.getMonth() + 1, 0)
-    
-        const startDayOfCurrentMonth : number = firstDayOfThisMonth.getDay()
-        const lastDateOfPrevMonth : number = new Date(2021, contextDate.getMonth(), 0).getDate()
-    
-        if(monthNameElement) {
-            monthNameElement.innerHTML = dayArray[contextMonth] + ` ${contextYear}`
+
+        contextDate.setMonth(contextMonth);
+        contextDate.setFullYear(contextYear);
+
+        const firstDayOfThisMonth: Date = new Date(
+            contextYear,
+            contextDate.getMonth(),
+            1
+        );
+        const lastDayOfThisMonth: Date = new Date(
+            contextYear,
+            contextDate.getMonth() + 1,
+            0
+        );
+
+        const startDayOfCurrentMonth: number = firstDayOfThisMonth.getDay();
+        const lastDateOfPrevMonth: number = new Date(
+            2021,
+            contextDate.getMonth(),
+            0
+        ).getDate();
+
+        if (monthNameElement) {
+            monthNameElement.innerHTML =
+                dayArray[contextMonth] + ` ${contextYear}`;
         }
-        if(currentDateElement) {
-            currentDateElement.innerHTML = new Date().toDateString()
+        if (currentDateElement) {
+            currentDateElement.innerHTML = new Date().toDateString();
         }
-    
-        if(datesElement) {
-            datesElement.innerHTML = ''
-            for(let i = lastDateOfPrevMonth - startDayOfCurrentMonth + 1; i<=lastDateOfPrevMonth; i++){
-                datesElement.innerHTML += `<div class='Date PrevDate'>${i}</div>`
+
+        if (datesElement) {
+            datesElement.innerHTML = "";
+            for (
+                let i = lastDateOfPrevMonth - startDayOfCurrentMonth + 1;
+                i <= lastDateOfPrevMonth;
+                i++
+            ) {
+                datesElement.innerHTML += `<div class='Date PrevDate'>${i}</div>`;
             }
-            for(let i = 1; i<=lastDayOfThisMonth.getDate(); i++) {
-                let eventsFlag = false
-                const today = new Date(contextYear, contextMonth, i)
-                const todayString = today.toDateString()
-                const numberSpan = `<span class='Number'>${i}</span>`
-    
-                let redEvent : string = ''
-                let greenEvent : string = ''
-                let blueEvent : string = ''
-    
-                if(eventsArray[todayString]) {
-                    console.log('Matched')
-                    eventsFlag = true
-                    if(eventsArray[todayString].red) {
-                        redEvent = `<span class='EventsElement Red'></span><span class='Hover NoDisplay'>${eventsArray[todayString].red}</span>`
+            for (let i = 1; i <= lastDayOfThisMonth.getDate(); i++) {
+                let eventsFlag = false;
+                const today = new Date(contextYear, contextMonth, i);
+                const todayString = today.toDateString();
+                const numberSpan = `<span class='Number'>${i}</span>`;
+
+                let redEvent: string = "";
+                let greenEvent: string = "";
+                let blueEvent: string = "";
+
+                if (eventsArray[todayString]) {
+                    console.log("Matched");
+                    eventsFlag = true;
+                    if (eventsArray[todayString].red) {
+                        redEvent = `<span class='EventsElement Red'></span><span class='Hover NoDisplay'>${eventsArray[todayString].red}</span>`;
                     }
-                    if(eventsArray[todayString].blue) {
-                        greenEvent = `<span class='EventsElement Blue'></span><span class='Hover NoDisplay'>${eventsArray[todayString].blue}</span>`
+                    if (eventsArray[todayString].blue) {
+                        greenEvent = `<span class='EventsElement Blue'></span><span class='Hover NoDisplay'>${eventsArray[todayString].blue}</span>`;
                     }
-                    if(eventsArray[todayString].green) {
-                        blueEvent = `<span class='EventsElement Green'></span><span class='Hover NoDisplay'>${eventsArray[todayString].green}</span>`
+                    if (eventsArray[todayString].green) {
+                        blueEvent = `<span class='EventsElement Green'></span><span class='Hover NoDisplay'>${eventsArray[todayString].green}</span>`;
                     }
                 }
                 // eventsArray
-    
-                
-                const eventsDiv = `<div class='Events'>${redEvent ? redEvent : ''}${greenEvent ? greenEvent : ''}${blueEvent ? blueEvent : ''}</div>`
-                datesElement.innerHTML += `<div class='Date ${i === new Date().getDate() && new Date().getMonth() === contextMonth && new Date().getFullYear() === contextYear ? 'Active' : ''}'>${numberSpan}${eventsFlag ? eventsDiv : ''}</div>`
+
+                const eventsDiv = `<div class='Events'>${
+                    redEvent ? redEvent : ""
+                }${greenEvent ? greenEvent : ""}${
+                    blueEvent ? blueEvent : ""
+                }</div>`;
+                datesElement.innerHTML += `<div class='Date ${
+                    i === new Date().getDate() &&
+                    new Date().getMonth() === contextMonth &&
+                    new Date().getFullYear() === contextYear
+                        ? "Active"
+                        : ""
+                }'>${numberSpan}${eventsFlag ? eventsDiv : ""}</div>`;
             }
-            for(let i = 1; i<= 7 - lastDayOfThisMonth.getDay() - 1; i++) {
-                datesElement.innerHTML += `<div class='Date NextDate'>${i}</div>`
+            for (let i = 1; i <= 7 - lastDayOfThisMonth.getDay() - 1; i++) {
+                datesElement.innerHTML += `<div class='Date NextDate'>${i}</div>`;
             }
         }
-    
-        document.querySelectorAll('.EventsElement').forEach(item => {
-            item.addEventListener('mouseenter', () => {
-                item.nextElementSibling?.classList.remove('NoDisplay')
-            })
-        })
-        document.querySelectorAll('.EventsElement').forEach(item => {
-            item.addEventListener('mouseout', () => {
-                item.nextElementSibling?.classList.add('NoDisplay')
-            })
-        })
+
+        document.querySelectorAll(".EventsElement").forEach((item) => {
+            item.addEventListener("mouseenter", () => {
+                item.nextElementSibling?.classList.remove("NoDisplay");
+            });
+        });
+        document.querySelectorAll(".EventsElement").forEach((item) => {
+            item.addEventListener("mouseout", () => {
+                item.nextElementSibling?.classList.add("NoDisplay");
+            });
+        });
     };
-    
-    let monthCounter = new Date().getMonth()
-    let yearCounter = new Date().getFullYear()
-    
+
+    let monthCounter = new Date().getMonth();
+    let yearCounter = new Date().getFullYear();
+
     renderCalendar(monthCounter, yearCounter);
-    
-    
-    
-    const prevMonthChevron = document.querySelector('#prevMonth')
-    const nextMonthChevron = document.querySelector('#nextMonth')
-    
-    if(prevMonthChevron) {
-        prevMonthChevron.addEventListener('click', () => {
-            monthCounter--
-            if(monthCounter<0) {
-                yearCounter--
-                monthCounter = 11
+
+    const prevMonthChevron = document.querySelector("#prevMonth");
+    const nextMonthChevron = document.querySelector("#nextMonth");
+
+    if (prevMonthChevron) {
+        prevMonthChevron.addEventListener("click", () => {
+            monthCounter--;
+            if (monthCounter < 0) {
+                yearCounter--;
+                monthCounter = 11;
             }
-            renderCalendar(monthCounter, yearCounter)
-        })
+            renderCalendar(monthCounter, yearCounter);
+        });
     }
-    
-    if(nextMonthChevron) {
-        nextMonthChevron.addEventListener('click', () => {
-            monthCounter++
-            if(monthCounter>11) {
-                yearCounter++
-                monthCounter = 0
+
+    if (nextMonthChevron) {
+        nextMonthChevron.addEventListener("click", () => {
+            monthCounter++;
+            if (monthCounter > 11) {
+                yearCounter++;
+                monthCounter = 0;
             }
-            renderCalendar(monthCounter, yearCounter)
-        })
+            renderCalendar(monthCounter, yearCounter);
+        });
     }
-}
+};
